3d2a6160ffae111f13dc055ac86fd3fcf9944b74
Nursultan1 <emirjusup@gmail.com>
2021-06-23 19:49:44 +0300

    Initial commit

---
+++ app/src/main/java/com/io/hw_6_an3/MathMethod.java
@@ -0,0 +1,22 @@
+package com.io.hw_6_an3;
+
+public class MathMethod {  public int add(int a, int b) {
+    return a + b;
+}
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) throws ArithmeticException {
+        if (b == 0)
+            return 0;
+        else
+        if (a == 0) return 0;
+        return a / b;
+    }
+}
